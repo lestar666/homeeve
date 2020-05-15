4b269f990a2e7e92256ab066f33e626de2aaf21a
2077946749 <2077946749@qq.com>
2020-05-15 14:20:00 +0800

    1

---
+++ equaly/equaly.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>  
+#include <opencv2/opencv.hpp>  
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat m = imread("etest.jpg");
+
+	vector<Mat> channels;//定义存储的容器
+	split(m, channels);
+
+	Mat dst;
+	Mat b = channels[0];//blue通道的图像
+	equalizeHist(b, b);//均衡化
+
+	Mat g = channels[1];
+	equalizeHist(g, g);
+
+	Mat r = channels[2];
+	equalizeHist(r, r);
+
+	merge(channels, dst);//合并通道
+
+	imshow("src", m);
+
+	imshow("dst", dst);
+	waitKey(0);
+	return 0;
+}
